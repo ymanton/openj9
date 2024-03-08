9afdf469e68d172a486afd80952d6d423b5ef572
Tobi Ajila <atobia@ca.ibm.com>
2024-03-08 11:52:15 -0500

    Disclaim ROM and RAM classes during checkpoint
    Disclaiming classes during checkpoint will lead to less memory
    usage during restore since all class memory might not be needed
    during restore.
    
    New JVM option: -XX:[+|-]CRIUDisclaimClasses
    Enabling new tracepoint: -Xtrace:print={j9vm.760}
    
    Co-authored-by: Tobi Ajila <atobia@ca.ibm.com>
    Co-authored-by: Babneet Singh <sbabneet@ca.ibm.com>
    Co-authored-by: Amarpreet Singh <Amarpreet.A.Singh@ibm.com>
    Signed-off-by: Babneet Singh <sbabneet@ca.ibm.com>
    (cherry picked from commit f696461138f9084307cdff90fbe04ad464939716)
    
    Call allClassLoadersEndDo to release the lock
    
    Co-authored-by: Tobi Ajila <atobia@ca.ibm.com>
    Signed-off-by: Amarpreet Singh <Amarpreet.A.Singh@ibm.com>
    (cherry picked from commit 01d1a98c1bc5cb8f51c6093eee741dc46001f720)
    
    Take J9VMThread and add noenv to the tracepoint
    
    Co-authored-by: Tobi Ajila <atobia@ca.ibm.com>
    Signed-off-by: Amarpreet Singh <Amarpreet.A.Singh@ibm.com>
    (cherry picked from commit b01187f79c8615f407c0dfc26a19c02217f6c8c1)
    
    Delete the first one
    
    Co-authored-by: Tobi Ajila <atobia@ca.ibm.com>
    Signed-off-by: Amarpreet Singh <Amarpreet.A.Singh@ibm.com>
    (cherry picked from commit 3287b02e7f372173be73d7f59a834ff570ef4ca0)

--- runtime/vm/intfunc.c
+++ runtime/vm/intfunc.c
@@ -426,6 +426,7 @@ J9InternalVMFunctions J9InternalFunctions = {
 	getRestoreSystemProperites,
 	setupJNIFieldIDsAndCRIUAPI,
 	criuCheckpointJVMImpl,
+	disclaimAllClassMemory,
 #endif /* defined(J9VM_OPT_CRIU_SUPPORT) */
 	getClassNameString,
 	getDefaultValueSlotAddress,
