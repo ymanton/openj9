20af5f3c137f564582093669e65c3a737e04fd37
Daryl Maier <maier@ca.ibm.com>
2024-04-29 15:53:44 -0400

    Remove deprecated shouldAlignTLHAlloc() checks on TR::Nodes
    No longer set nor used anywhere in OMR or OpenJ9.
    
    Signed-off-by: Daryl Maier <maier@ca.ibm.com>
    (cherry picked from commit 6f2bb914e300fc529a922af561cd249d0f3f80a2)

--- runtime/compiler/x/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/x/codegen/J9TreeEvaluator.cpp
@@ -6184,7 +6184,7 @@ static void genHeapAlloc(
 
 #if defined(J9VM_GC_THREAD_LOCAL_HEAP)
          if ((node->getOpCodeValue() == TR::New) &&
-             (comp->getMethodHotness() >= hot || node->shouldAlignTLHAlloc()) &&
+             (comp->getMethodHotness() >= hot) &&
              !disableAllocationAlignment)
             {
             TR_OpaqueMethodBlock *ownMethod = node->getOwningMethod();
@@ -6578,7 +6578,7 @@ static void genHeapAlloc2(
 
 #if defined(J9VM_GC_THREAD_LOCAL_HEAP)
          if ((node->getOpCodeValue() == TR::New) &&
-             (comp->getMethodHotness() >= hot || node->shouldAlignTLHAlloc()) &&
+             (comp->getMethodHotness() >= hot) &&
              !disableAllocationAlignment)
             {
             TR_OpaqueMethodBlock *ownMethod = node->getOwningMethod();
