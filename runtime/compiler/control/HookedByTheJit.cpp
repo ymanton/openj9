453d351b83acc2e9548f7aeb39b3b870123e4e76
Younes Manton <ymanton@ca.ibm.com>
2024-10-29 11:00:28 -0700

    Fix rss delta print

--- runtime/compiler/control/HookedByTheJit.cpp
+++ runtime/compiler/control/HookedByTheJit.cpp
@@ -4444,7 +4444,7 @@ void disclaimClassMemory(J9JavaVM *javaVM, uint64_t crtElapsedTime)
    javaVM->internalVMFunctions->disclaimAllClassMemory(javaVM);
    size_t rssAfter = getRSS_Kb();
    if (TR::Options::getCmdLineOptions()->getVerboseOption(TR_VerbosePerformance))
-      TR_VerboseLog::writeLineLocked(TR_Vlog_PERF, "t=%u JIT disclaimed class memory RSS before=%zu KB, RSS after=%zu KB, delta=%zu KB = %5.2f%%",
+      TR_VerboseLog::writeLineLocked(TR_Vlog_PERF, "t=%u JIT disclaimed class memory RSS before=%zu KB, RSS after=%zu KB, delta=%zd KB = %5.2f%%",
                                      (uint32_t)crtElapsedTime, rssBefore, rssAfter, rssBefore - rssAfter, ((long)(rssAfter - rssBefore) * 100.0 / rssBefore));
    }
 
@@ -4454,7 +4454,7 @@ void disclaimDataCaches(uint64_t crtElapsedTime)
    int numDisclaimed = TR_DataCacheManager::getManager()->disclaimAllDataCaches();
    size_t rssAfter = getRSS_Kb();
    if (TR::Options::getCmdLineOptions()->getVerboseOption(TR_VerbosePerformance))
-      TR_VerboseLog::writeLineLocked(TR_Vlog_PERF, "t=%u JIT disclaimed %d Data Cache segments  RSS before=%zu KB, RSS after=%zu KB, delta=%zu KB",
+      TR_VerboseLog::writeLineLocked(TR_Vlog_PERF, "t=%u JIT disclaimed %d Data Cache segments  RSS before=%zu KB, RSS after=%zu KB, delta=%zd KB",
                                      (uint32_t)crtElapsedTime, numDisclaimed, rssBefore, rssAfter, rssBefore - rssAfter);
    }
 
@@ -4468,7 +4468,7 @@ void disclaimIProfilerSegments(uint64_t crtElapsedTime)
       int numSegDisclaimed = iprofilerAllocator->disclaimAllSegments();
       size_t rssAfter = getRSS_Kb();
       if (TR::Options::getCmdLineOptions()->getVerboseOption(TR_VerbosePerformance))
-         TR_VerboseLog::writeLineLocked(TR_Vlog_PERF, "t=%u JIT disclaimed %d IProfiler segments out of %d. RSS before=%zu KB, RSS after=%zu KB, delta=%zu KB",
+         TR_VerboseLog::writeLineLocked(TR_Vlog_PERF, "t=%u JIT disclaimed %d IProfiler segments out of %d. RSS before=%zu KB, RSS after=%zu KB, delta=%zd KB",
                                         (uint32_t)crtElapsedTime, numSegDisclaimed, iprofilerAllocator->getNumSegments(), rssBefore, rssAfter, rssBefore - rssAfter);
       }
    }
@@ -4479,7 +4479,7 @@ void disclaimCodeCaches(uint64_t crtElapsedTime)
    int numDisclaimed = TR::CodeCacheManager::instance()->disclaimAllCodeCaches();
    size_t rssAfter = getRSS_Kb();
    if (TR::Options::getCmdLineOptions()->getVerboseOption(TR_VerbosePerformance))
-      TR_VerboseLog::writeLineLocked(TR_Vlog_PERF, "t=%u JIT disclaimed %d Code Caches RSS before=%zu KB, RSS after=%zu KB, delta=%zu KB = %5.2f%%",
+      TR_VerboseLog::writeLineLocked(TR_Vlog_PERF, "t=%u JIT disclaimed %d Code Caches RSS before=%zu KB, RSS after=%zu KB, delta=%zd KB = %5.2f%%",
                                      (uint32_t)crtElapsedTime, numDisclaimed, rssBefore, rssAfter, rssBefore - rssAfter, ((long)(rssAfter - rssBefore) * 100.0 / rssBefore));
    }
 
@@ -4527,7 +4527,7 @@ void memoryDisclaimLogic(TR::CompilationInfo *compInfo, uint64_t crtElapsedTime,
 #if defined(J9VM_OPT_SHARED_CLASSES)
    TR_J9VMBase *fej9 = TR_J9VMBase::get(jitConfig, compInfo->getSamplerThread(), TR_J9VMBase::AOT_VM);
    TR_J9SharedCache *sharedCache = fej9->sharedCache();
-   if (sharedCache->isDisclaimEnabled())
+   if (sharedCache && sharedCache->isDisclaimEnabled())
       {
       // Disclaim if there was a large time interval since the last disclaim
       if (crtElapsedTime > lastSCCDisclaimTime + 12 * TR::Options::_minTimeBetweenMemoryDisclaims)
